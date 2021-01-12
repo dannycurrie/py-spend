84d6f2554b57a4cb4f636a052e5f1de0f54fd0d2
dannycurrie <danny.currie42@gmail.com>
2021-01-12 21:05:25 +0000

    calculate totals

--- spend.py
+++ spend.py
@@ -3,10 +3,14 @@ import os
 
 categoryNames = os.listdir('./categories')
 categories = {}
+totals = {
+    'none': 0
+}
 
 for category in categoryNames:
     with open('./categories/' + category) as f:
         categories[category] = list(map(str.strip, f.readlines()))
+        totals[category] = 0
 
 
 def getCategory(desc):
@@ -21,7 +25,6 @@ def isExpenditure(row):
     try:
         return float(row[2]) < 0
     except:
-        print(row)
         return False
 
 
@@ -32,12 +35,15 @@ with open('./put-data-here/data-cleaned.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         if(isExpenditure(row)):
+            category = getCategory(row[1])
+            value = float(row[2]) * -1
             parsedRows.append({
                 'date': row[0],
                 'desc': row[1],
                 'value': float(row[2]),
-                'category': getCategory(row[1])
+                'category': category
             })
+            totals[category] += value
 
 with open('./output/output.csv', mode='w') as output_file:
     csv_writer = csv.DictWriter(
@@ -45,3 +51,13 @@ with open('./output/output.csv', mode='w') as output_file:
         fieldnames=['date', 'desc', 'value', 'category']
     )
     csv_writer.writerows(parsedRows)
+
+with open('./output/totals.csv', mode='w') as totals_file:
+    csv_writer = csv.writer(
+        totals_file,
+        delimiter=',',
+        quotechar='"',
+        quoting=csv.QUOTE_MINIMAL
+    )
+    for category in totals.keys():
+        csv_writer.writerow([category, totals[category]])
